19a32c161558b20b5204983f3c71467fbd0199b2
BpHana <57785153+BpHana@users.noreply.github.com>
2021-10-30 14:49:54 +0700

    .
    Init project

---
+++ api/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+from api.model import Image
+
+admin.site.register(Image)
---
+++ api/views.py
@@ -0,0 +1,24 @@
+import logging
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .serializers import ImageSerializer
+from .model import Image
+from yolov5 import detect
+from mafia_api.settings import MEDIA_ROOT, BASE_DIR
+
+
+# Create your views here.
+class ImageView(APIView):
+
+    def post(self, request):
+        serializer = ImageSerializer(data=request.data)
+
+        if serializer.is_valid():
+            file = serializer.save()
+            detect.run(source=MEDIA_ROOT / file.image.name, weights=BASE_DIR / 'yolov5/weights/best-2110-midnight.pt',
+                       project=MEDIA_ROOT / 'detect', imgsz=416, conf_thres=0.4)
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
