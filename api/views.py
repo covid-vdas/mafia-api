fd13d690235dccdf34e364e636d4f5049dbc1335
BpHana <57785153+BpHana@users.noreply.github.com>
2021-10-30 15:35:49 +0700

    delete exp dir

--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 import logging
+import shutil
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -17,8 +18,9 @@ class ImageView(APIView):
 
         if serializer.is_valid():
             file = serializer.save()
+            shutil.rmtree(MEDIA_ROOT / 'detect/exp')
             detect.run(source=MEDIA_ROOT / file.image.name, weights=BASE_DIR / 'yolov5/weights/best-2110-midnight.pt',
                        project=MEDIA_ROOT / 'detect', imgsz=416, conf_thres=0.4)
-            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+            return Response({"status": "success", "data": serializer.data}, status=status   .HTTP_200_OK)
         else:
             return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
